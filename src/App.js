e9cc31a8da8da5c83a5e52b5f35b1bed831cccba
Ramsai <ramsaicrony@gmail.com>
2023-05-08 22:24:25 +0530

    Added buttons and NavLink to highlight clicked button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Support } from './components/Support';
 import { About } from './components/About';
 import { NotFound } from './components/NotFound';
 import { Labs } from './components/Labs';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/support">Support</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/labs">Labs</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/support" element={<Support/>} />
